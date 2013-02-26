1b814cc60a2fbc70b4e5c72b0fb84003085caa13
Andrius Bolsaitis <andrius.bolsaitis@gmail.com>
2013-02-26 21:55:31 +0200

    Ruby mine commit

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,7 +30,7 @@ describe "Authentication" do
 			describe "followed by signout" do
 				before { click_link "Sign out" }
 				it { should have_link('Sign in') }
-			end
+      end
 		end
-	end
+  end
 end

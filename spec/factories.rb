8cbd9baadeb2418c9a507136e3c36bc19b46fd17
Andrius Bolsaitis <andrius.bolsaitis@gmail.com>
2013-03-24 17:25:48 +0200

    Finished chapter 9.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name "Petras Petraitis"
-		email "petras@gmail.com"
-		password "koja123"
-		password_confirmation "koja123"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com" }
+		password "foobar"
+		password_confirmation "foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
